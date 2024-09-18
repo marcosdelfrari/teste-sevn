c7bd8e32e4c3cdf28ec51cc9d3030edbf1812835
Marcos Lucas <mac@192.168.100.8>
2024-09-18 18:33:40 -0300

    tratamento para sem escudo

--- main.js
+++ main.js
@@ -12,6 +12,16 @@ const versus = `<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmln
 <path d="M1 1L13 13" stroke="#D1D1D1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
 `;
+const escudoNan = `<svg width="32" height="40" viewBox="0 0 24 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M12 0C11.1627 1.12221 10.6201 3.23737 7.15872 3.76028C6.8344 3.80729 6.52776 3.83079 6.23292 3.83079C4.03931 3.83079 2.71253 2.63807 2.71253 2.63807L0 5.48766C0 5.48766 4.19853 6.82726 0.837346 18.8367C-1.43882 26.9683 10.9445 28.2021 12 30C13.0496 28.2021 25.4329 26.9683 23.1627 18.8367C19.8074 6.82726 24 5.48766 24 5.48766L21.2816 2.63807C21.2816 2.63807 19.9548 3.83079 17.7612 3.83079C17.4663 3.83079 17.1597 3.80729 16.8354 3.76028C13.3799 3.24324 12.8373 1.12221 11.9941 0L12 0Z" fill="url(#paint0_linear_417_2)"/>
+<defs>
+<linearGradient id="paint0_linear_417_2" x1="12" y1="0" x2="12" y2="30" gradientUnits="userSpaceOnUse">
+<stop stop-color="#CDCDCD"/>
+<stop offset="1" stop-color="#A5A5A5"/>
+</linearGradient>
+</defs>
+</svg>
+`;
 
 async function updateContent() {
   try {
@@ -92,7 +102,7 @@ async function updateContent() {
       console.log(
         "Validação do escudo por conta de cache do chrome" + teamName + svg
       );
-      return svg ? `${svg}` : ``;
+      return svg ? `${svg}` : `${escudoNan}`;
     }
 
     renderRodadas(rodadaAtual);
